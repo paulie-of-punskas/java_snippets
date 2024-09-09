55e6de13297219327b121209512bcf0fe311cfd7
paulie-of-punskas <pbernecki@protonmail.com>
2024-09-09 11:04:41 +0200

    file read and print

---
+++ read_print_file/src/FileUtils.java
@@ -0,0 +1,23 @@
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileUtils {
+    public void printFileContent(String file_path) {
+        // Method checks if exists and prints it. Otherwise, prints `false`.
+        Path path = Paths.get(file_path);
+        if (Files.exists(path)) {
+            try {
+                List<String> readFileContent = new ArrayList<>(Files.readAllLines(path));
+                readFileContent.forEach(System.out::println);
+            } catch (Exception e) {
+                System.out.println("Exception occurred: " + e);
+            }
+        } else {
+            System.out.println(file_path + " does not exist");
+        }
+    }
+}
+
---
+++ read_print_file/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        FileUtils fileRead = new FileUtils();
+        fileRead.printFileContent("./data/test.txt");
+        fileRead.printFileContent("./data/te2st.txt");
+    }
+}
